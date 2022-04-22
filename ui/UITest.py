1214e802a2a37baea5bc215540c76e1ea276c786
johab2408 <josef.habich@student.tugraz.at>
2022-04-22 19:11:19 +0200

    basic ui functions

--- ui/UITest.py
+++ ui/UITest.py
@@ -1,5 +1,24 @@
 import streamlit as st
 import numpy as np
+import pathlib as pl
+import pandas as pd
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+# importing sys
+import sys
+
+#IMPORTANT: set right path:
+#Enter the folloing into Terminal
+#    export PYTHONPATH='path/to/NLP/project' ####
+
+from model import MLModel as ml
+
+
+txt = st.text_area('Text to analyze', '')
+
+
+if st.button('Init Model'):
+    ml.init()
+
+if st.button('Analyse Text Model'):
+     ml.analyse_text(txt)
+
