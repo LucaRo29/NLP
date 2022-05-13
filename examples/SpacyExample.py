0eb37d79ed183bd79eb5dc3d9956b691df4feda3
johab2408 <josef.habich@student.tugraz.at>
2022-05-13 08:10:55 +0200

    new data

--- examples/SpacyExample.py
+++ examples/SpacyExample.py
@@ -12,114 +12,97 @@ from matplotlib import pyplot as plt
 
 ## Paths
 data_path = "../data/spam.csv"
+data_path2 = "../data/enron_spam_data.csv"
 
-## Configurations
-sns.set(style="darkgrid")
+from string import punctuation
+import re
 
-## UDF's
-def train_model(model, train_data, optimizer, batch_size, epochs=10):
-    losses = {}
-    random.seed(1)
+from nltk.stem.snowball import SnowballStemmer
+# nltk.download('wordnet') # uncomment to download 'wordnet'
+from nltk.corpus import wordnet as wn
 
-    for epoch in range(epochs):
-        random.shuffle(train_data)
 
-        batches = minibatch(train_data, size=batch_size)
-        for batch in batches:
-            # Split batch into texts and labels
-            texts, labels = zip(*batch)
+def clean_email(email):
+    """ Remove all punctuation, urls, numbers, and newlines.
+    Convert to lower case.
+    Args:
+        email (unicode): the email
+    Returns:
+        email (unicode): only the text of the email
+    """
+    email = re.sub(r'http\S+', ' ', email)
+    email = re.sub("\d+", " ", email)
+    email = email.replace('\n', ' ')
+    email = email.translate(str.maketrans("", "", punctuation))
+    email = email.lower()
+    return email
 
-            # Update model with texts and labels
-            model.update(texts, labels, sgd=optimizer, losses=losses)
-        # print("Loss: {}".format(losses['textcat']))
 
-    return losses['textcat']
 
-def get_predictions(model, texts):
-    # Use the model's tokenizer to tokenize each input text
-    docs = [model.tokenizer(text) for text in texts]
 
-    # Use textcat to get the scores for each doc
-    textcat = model.get_pipe('textcat')
-    scores, _ = textcat.predict(docs)
 
-    # From the scores, find the label with the highest score/probability
-    predicted_labels = scores.argmax(axis=1)
-    predicted_class = [textcat.labels[label] for label in predicted_labels]
+def preproces_text(email):
+    """ Split the text string into individual words, stem each word,
+    and append the stemmed word to words. Make sure there's a single
+    space between each stemmed word.
+    Args:
+        email (unicode): the email
+    Returns:
+        words (unicode): the text of the email
+    """
 
-    return predicted_class
+    words = ""
+    # Create the stemmer.
+    stemmer = SnowballStemmer("english")
+    # Split text into words.
+    email = email.split()
+    for word in email:
+        # Optional: remove unknown words.
+        # if wn.synsets(word):
+        words = words + stemmer.stem(word) + " "
+
+    return words
+
 
 
 ######## Main method ########
 
 def main():
 
-    # Load dataset
-    data = pd.read_csv(data_path,encoding='latin-1')
-    observations = len(data.index)
-    # print("Dataset Size: {}".format(observations))
 
-    # Create an empty spacy model
-    nlp = spacy.blank("en")
+    merged_data = pd.DataFrame(columns=["label", "text"])
 
-    # Create the TextCategorizer with exclusive classes and "bow" architecture
-    text_cat = nlp.create_pipe(
-                  "textcat",
-                  config={
-                    "exclusive_classes": True,
-                    "architecture": "bow"})
+    # Load dataset 1
+    dataset1 = pd.read_csv(data_path,encoding='latin-1')
+    print(dataset1.head())
+    print(dataset1.columns.values)
+    dataset1.drop(columns=['Unnamed: 2', 'Unnamed: 3','Unnamed: 4'],inplace=True)
+    print(dataset1.columns.values)
 
-    # Adding the TextCategorizer to the created empty model
-    nlp.add_pipe(text_cat)
+    # Load dataset 2
+    dataset2 = pd.read_csv(data_path2, encoding='latin-1')
+    print(dataset2.head())
+    print(dataset2.columns.values)
 
-    # Add labels to text classifier
-    text_cat.add_label("ham")
-    text_cat.add_label("spam")
+    #Drop Nans
+    dataset2 = dataset2.dropna(subset=['Message'])
+    #clean data
 
-    # Split data into train and test datasets
-    x_train, x_test, y_train, y_test = train_test_split(
-        data['text'], data['label'], test_size=0.33, random_state=7)
+    #dataset2['Message'] = dataset2['Message'].apply(clean_email)
 
-    # Create the train and test data for the spacy model
-    train_lables = [{'cats': {'ham': label == 'ham',
-                              'spam': label == 'spam'}}  for label in y_train]
-    test_lables = [{'cats': {'ham': label == 'ham',
-                          'spam': label == 'spam'}}  for label in y_test]
+    #prep data
 
-    # Spacy model data
-    train_data = list(zip(x_train, train_lables))
-    test_data = list(zip(x_test, test_lables))
+    #dataset2['Message'] = dataset2['Message'].apply(preproces_text)
 
-    # Model configurations
-    optimizer = nlp.begin_training()
-    batch_size = 5
-    epochs = 10
+    dataset2.drop(columns=['Message ID', 'Subject','Date'],inplace=True)
+    dataset2.rename(columns={"Message": "text", "Spam/Ham": "label"})
+    dataset2 = dataset2.reindex(columns=["label", "text"])
+    print(dataset2.columns.values)
 
-    # Training the model
-    train_model(nlp, train_data, optimizer, batch_size, epochs)
-
-    # Sample predictions
-    # print(train_data[0])
-    # sample_test = nlp(train_data[0][0])
-    # print(sample_test.cats)
-
-    # Train and test accuracy
-    train_predictions = get_predictions(nlp, x_train)
-    test_predictions = get_predictions(nlp, x_test)
-    train_accuracy = accuracy_score(y_train, train_predictions)
-    test_accuracy = accuracy_score(y_test, test_predictions)
-
-    print("Train accuracy: {}".format(train_accuracy))
-    print("Test accuracy: {}".format(test_accuracy))
-
-    # Creating the confusion matrix graphs
-    cf_train_matrix = confusion_matrix(y_train, train_predictions)
-    plt.figure(figsize=(10,8))
-    sns.heatmap(cf_train_matrix, annot=True, fmt='d')
-
-    cf_test_matrix = confusion_matrix(y_test, test_predictions)
-    plt.figure(figsize=(10,8))
-    sns.heatmap(cf_test_matrix, annot=True, fmt='d')
+    frames = [dataset1,dataset2]
+    result = pd.concat(frames)
+    print(result.columns.values)
+    print(result.head())
 
 
 if __name__ == "__main__":
